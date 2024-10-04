34ba0c77e261677c69a18d1525ac52ae8faf68e8
orion <nikolaoskor8@gmail.com>
2024-10-04 21:00:21 +0300

    project 1.1 init

--- project1/Project1.1.X/main.asm
+++ project1/Project1.1.X/main.asm
@@ -1,3 +1,12 @@
+.include "m328PBdef.inc"
+.def temp=r16
+    
+init_stack:
+    ldi temp, low(RAMEND)	; initialize stack pointer
+    out SPL, temp
+    ldi temp, high(RAMEND)
+    out SPH, temp
 
-
+init_ports:
+    
 
